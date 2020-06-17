170ee1161c8fa29dfb476068882441e81a5bbe57
Cal Corbin <calcorbin@Cals-MacBook-Pro.local>
2020-06-17 10:05:42 -0500

    Add construction placeholder page

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="construction-font">
+        <p>
+          Currently under construction.
+        </p>
+        <a
+          className="App-link"
+          href="https://www.linkedin.com/in/calcorbin/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Reach me here for now.
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
