be4c62d80356f241ed389d1ecad6c88662afbb2c
Cal Corbin <calcorbin@Cals-MacBook-Pro.local>
2020-06-18 10:39:51 -0500

    Change app component to pure function

--- src/components/construction-sign/index.jsx
+++ src/components/construction-sign/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './styles.css';
 
-export default function ConstructionSign(props) {
+export default function ConstructionSign() {
   return (
     <div>
       <div
