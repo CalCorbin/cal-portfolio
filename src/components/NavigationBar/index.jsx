7b7e4590796f7f76338c7285dea04800bfae1af9
Cal Corbin <calcorbin@gmail.com>
2021-10-20 16:29:16 -0500

    Create navigation bar rough design

--- src/components/NavigationBar/index.jsx
+++ src/components/NavigationBar/index.jsx
@@ -1,29 +1,20 @@
 import React from 'react';
 import './styles.css';
 
-function loadPage(url) {}
-
 export default function NavigationBar() {
   return (
     <div className="__navigation-bar" data-testid="navigation-bar">
-      <div className="__button-container">
-        <button
-          type="button"
-          className="__button"
-          data-testid="about-button"
-          onClick={loadPage('/about')}
-        >
-          ABOUT
-        </button>
-        <button
-          type="button"
-          className="__button"
-          data-testid="resume-button"
-          onClick={loadPage('/about')}
-        >
-          RESUME
-        </button>
+      <div className="__hamburger">
+        <div className="__menu-dash" />
+        <div className="__menu-dash" />
+        <div className="__menu-dash" />
       </div>
+      <button type="button" className="__button" data-testid="resume-button">
+        RESUME
+      </button>
+      <button type="button" className="__button" data-testid="about-button">
+        ABOUT
+      </button>
     </div>
   );
 }
