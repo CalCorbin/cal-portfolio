f092064a6c7dfeb6d03001a4d3615c7ed897ee9d
Cal Corbin <calcorbin@gmail.com>
2021-10-25 10:21:09 -0500

    Fix broken NavigationBar test

--- src/components/NavigationBar/NavigationBar.test.js
+++ src/components/NavigationBar/NavigationBar.test.js
@@ -3,16 +3,14 @@ import { cleanup, render, fireEvent, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import NavigationBar from './NavigationBar';
 
+beforeEach(() => render(<NavigationBar />));
 afterEach(cleanup);
 
 it('should render the Navigation bar', () => {
-  const { getByTestId } = render(<NavigationBar />);
-  expect(getByTestId('Navigation-bar')).toHaveClass('__navigation-bar');
+  expect(screen.getByTestId('navigation-bar')).toBeInTheDocument();
 });
 
 it('should render the resume button', () => {
-  render(<NavigationBar />);
-
   const resumeButton = screen.getByTestId('resume-button');
 
   fireEvent.click(resumeButton);
