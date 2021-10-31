3b68b5a910fb7e04f6e8d5dee0489a3f391c9c85
Cal Corbin <calcorbin@gmail.com>
2021-10-31 15:55:26 -0500

    Wrap landing page redesign

--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import ROUTES from '../../constants/routes';
 import './Landing.css';
 
@@ -12,9 +14,29 @@ function LandingPage() {
     <div data-testid="landing-page" className="landing">
       <h1 data-testid="header-title">cal corbin</h1>
       <h2>software engineer</h2>
+      <div className="social">
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://github.com/CalCorbin"
+        >
+          <FontAwesomeIcon size="2x" icon={faGithub} className="social-icon" />
+        </a>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.linkedin.com/in/calcorbin/"
+        >
+          <FontAwesomeIcon
+            size="2x"
+            icon={faLinkedin}
+            className="social-icon"
+          />
+        </a>
+      </div>
       <button
-        className="resume-button"
         type="button"
+        className="resume-button"
         aria-label="resume button"
         data-testid="resume-button"
         alt="view my resume/cv"
