9dba9556574be729e349617153bb4de229f9b9b7
Cal Corbin <calcorbin@gmail.com>
2021-11-08 16:59:27 -0600

    Display spacex ships

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import Firebase, { FirebaseContext } from './components/Firebase';
 import App from './App';
 import './index.css';
 
+const client = new ApolloClient({
+  uri: 'https://api.spacex.land/graphql/',
+  cache: new InMemoryCache(),
+});
+
 ReactDOM.render(
   <FirebaseContext.Provider value={new Firebase()}>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </FirebaseContext.Provider>,
   document.getElementById('root')
 );
--- src/pages/SpaceX/index.js
+++ src/pages/SpaceX/index.js
@@ -1,11 +1,60 @@
 import React from 'react';
+import { useQuery, gql } from '@apollo/client';
 import './SpaceX.css';
+import PropTypes from 'prop-types';
+
+const GET_SHIPS = gql`
+  query {
+    ships {
+      id
+      name
+      home_port
+      image
+    }
+  }
+`;
+
+function Ship({ ship }) {
+  return (
+    <div className="ship-card">
+      {ship.image ? (
+        <img src={ship.image} alt="spacex ship" />
+      ) : (
+        <div>No Image Available</div>
+      )}
+      <div>Name: {ship.name}</div>
+      <div>Home Port: {ship.home_port}</div>
+    </div>
+  );
+}
+
+Ship.defaultProps = {
+  ship: null,
+};
+
+Ship.propTypes = {
+  ship: PropTypes.shape({
+    image: PropTypes.string,
+    name: PropTypes.string,
+    home_port: PropTypes.string,
+  }),
+};
 
 function SpaceX() {
+  const { loading, error, data } = useQuery(GET_SHIPS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error...</p>;
+
   return (
-    <div data-testid="spacex-page">
-      <div className="section-header">SpaceX Spaceships</div>
+    <div data-testid="spacex-page" className="spacex-page">
+      <div className="section-header">SpaceX Ships</div>
       <hr />
+      <div className="ship-container">
+        {data.ships.map((ship) => (
+          <Ship key={ship.id} ship={ship} />
+        ))}
+      </div>
     </div>
   );
 }
