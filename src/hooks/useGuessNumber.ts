3b3b9340bd34db76607037d660a3923a452c5a2c
ccorbin <calcorbin@gmail.com>
2022-08-31 14:51:46 -0500

    Set up React Query for use with the ML endpoint

--- src/hooks/useGuessNumber.ts
+++ src/hooks/useGuessNumber.ts
@@ -1,19 +1,21 @@
 import { useMutation } from 'react-query';
 
-const guessNumber = async (imageFile: string) => {
+const guessNumber = async (imageFile: File | null) => {
+  if (!imageFile) return {};
+
+  const formData = new FormData();
+  formData.append('imageFile', imageFile);
+
   const response = await fetch(
     'http://127.0.0.1:8000/api/machine_learning_projects/predict-digit/',
     {
       method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ imageFile }),
+      body: formData,
     }
   );
   return response.json();
 };
 
-export default function useGuessNumber(imageFile: string) {
+export default function useGuessNumber(imageFile: File | null) {
   return useMutation(() => guessNumber(imageFile));
 }
