10acc2b8a17e66d646cb8a39c82b7d6df0c855c1
ccorbin <calcorbin@gmail.com>
2022-12-14 13:17:19 -0600

    Update constants and Card component to use typescript

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
-import Card from '../../components/Card';
+import Card from '../../components/Card/Card';
 import PROJECTS from '../../constants/projects';
 import './Landing.css';
 
