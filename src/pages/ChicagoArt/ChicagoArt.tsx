adfc9e0cb850e52190470feaed69cd3fb1190439
ccorbin <calcorbin@gmail.com>
2022-12-19 16:20:38 -0600

    Pull ArtCard into separate component

--- src/pages/ChicagoArt/ChicagoArt.tsx
+++ src/pages/ChicagoArt/ChicagoArt.tsx
@@ -2,51 +2,10 @@ import React, { useState } from 'react';
 import useSearchArtic from '../../hooks/useSearchArtic';
 import Header from '../../components/Header/Header';
 import Loading from '../../components/Loading';
+import ArtCard from './ArtCard';
 import { ArtProps } from './ChicagoArtInterface';
 import './ChicagoArt.css';
 
-const Art = ({
-  title,
-  artist_title: artistTitle,
-  artist_id: artistId,
-  image_id: imageId,
-  thumbnail,
-}: ArtProps) => {
-  const truncatedArtworkTitle =
-    title.length > 60 ? `${title.slice(0, 60)}...` : title;
-  return (
-    <div key={imageId} className="art" data-testid={`art-listing-${imageId}`}>
-      <img
-        src={`https://www.artic.edu/iiif/2/${imageId}/full/400,/0/default.jpg`}
-        alt={thumbnail.alt_text || title}
-      />
-      <div className="art-overlay">
-        <div className="art-title" data-testid={`art-listing-title-${imageId}`}>
-          {truncatedArtworkTitle}
-        </div>
-        {artistTitle ? (
-          <a
-            href={`https://www.artic.edu/artists/${artistId}/`}
-            className="artist"
-            target="_blank"
-            rel="noreferrer"
-            data-testid={`art-listing-artist-${imageId}`}
-          >
-            {artistTitle}
-          </a>
-        ) : (
-          <div
-            className="artist"
-            style={{ cursor: 'default' }}
-            data-testid={`artist-unknown-${imageId}`}
-          >
-            Artist Unknown
-          </div>
-        )}
-      </div>
-    </div>
-  );
-};
 const ChicagoArt = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const [enableSearch, setEnableSearch] = useState(false);
@@ -103,7 +62,7 @@ const ChicagoArt = () => {
       ) : (
         <div className="result-container">
           {art?.map((item: ArtProps) => (
-            <Art
+            <ArtCard
               key={item.image_id}
               title={item.title}
               artist_title={item.artist_title}
