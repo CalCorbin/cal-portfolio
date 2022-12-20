9341219dc3caffb609916c32123bbe2173d0f5b3
ccorbin <calcorbin@gmail.com>
2022-12-20 11:41:30 -0600

    Fix bug where image_id is null and image does not render

--- src/pages/ChicagoArt/ArtCard.tsx
+++ src/pages/ChicagoArt/ArtCard.tsx
@@ -7,37 +7,40 @@ const ArtCard = ({
   artist_id: artistId,
   image_id: imageId,
   thumbnail,
-}: ArtProps) => (
-  <div className="art" data-testid={`art-listing-${imageId}`}>
-    <img
-      src={`https://www.artic.edu/iiif/2/${imageId}/full/400,/0/default.jpg`}
-      alt={thumbnail?.alt_text || title}
-    />
-    <div className="art-overlay">
-      <div className="art-title" data-testid={`art-listing-title-${imageId}`}>
-        {title}
-      </div>
-      {artistTitle ? (
-        <a
-          href={`https://www.artic.edu/artists/${artistId}/`}
-          className="artist"
-          target="_blank"
-          rel="noreferrer"
-          data-testid={`art-listing-artist-${imageId}`}
-        >
-          {artistTitle}
-        </a>
-      ) : (
-        <div
-          className="artist-unknown"
-          style={{ cursor: 'default' }}
-          data-testid={`artist-unknown-${imageId}`}
-        >
-          Artist Unknown
+}: ArtProps) => {
+  if (!imageId) return null;
+  return (
+    <div className="art" data-testid={`art-listing-${imageId}`}>
+      <img
+        src={`https://www.artic.edu/iiif/2/${imageId}/full/400,/0/default.jpg`}
+        alt={thumbnail?.alt_text || title}
+      />
+      <div className="art-overlay">
+        <div className="art-title" data-testid={`art-listing-title-${imageId}`}>
+          {title}
         </div>
-      )}
+        {artistTitle ? (
+          <a
+            href={`https://www.artic.edu/artists/${artistId}/`}
+            className="artist"
+            target="_blank"
+            rel="noreferrer"
+            data-testid={`art-listing-artist-${imageId}`}
+          >
+            {artistTitle}
+          </a>
+        ) : (
+          <div
+            className="artist-unknown"
+            style={{ cursor: 'default' }}
+            data-testid={`artist-unknown-${imageId}`}
+          >
+            Artist Unknown
+          </div>
+        )}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default ArtCard;
