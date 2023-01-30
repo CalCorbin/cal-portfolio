77281bd2835c72c09811a6d285b0624ee563d46a
ccorbin <calcorbin@gmail.com>
2023-01-30 13:53:33 -0600

    Create function for getting neighbor count in mine cell

--- src/components/MineSweeper/Board.tsx
+++ src/components/MineSweeper/Board.tsx
@@ -18,24 +18,21 @@ const Board = ({ height, width, mines }: BoardProps) => {
     return minePlacements;
   };
   const minePositions = placeMines();
+
   const checkForMine = (x: number, y: number) =>
     minePositions.some((el) => el[0] === x && el[1] === y);
 
-  // const getNeighbourCount = (x: number, y: number) => {
-  //   let count = 0;
-  //   for (let i = -1; i < 2; i += 1) {
-  //     for (let j = -1; j < 2; j += 1) {
-  //       // Skip the current cell
-  //       if (i === 0 && j === 0) continue;
-  //       // Skip if out of bounds
-  //       if (x + i < 0 || x + i >= height || y + j < 0 || y + j >= width)
-  //         continue;
-  //       // Check if neighbour is a mine
-  //       if (minePositions.includes([x + i, y + j])) count += 1;
-  //     }
-  //   }
-  //   return count;
-  // };
+  const getNeighbourCount = (x: number, y: number) => {
+    let count = 0;
+    for (let i = -1; i <= 1; i += 1) {
+      for (let j = -1; j <= 1; j += 1) {
+        if (checkForMine(x + i, y + j) && (i !== 0 || j !== 0)) {
+          count += 1;
+        }
+      }
+    }
+    return count;
+  };
 
   return (
     <div data-testid="board-container">
@@ -49,15 +46,14 @@ const Board = ({ height, width, mines }: BoardProps) => {
             <div data-testid="board-row" className="mine-row" key={rowId}>
               {[...Array(width)].map((__, j) => {
                 const cellId = `row-${i}-cell-${j}`;
+                const neighborCount = getNeighbourCount(i, j);
+                console.log('-> neighborCount', neighborCount);
                 return (
                   <Cell
                     key={cellId}
-                    x={i}
-                    y={j}
                     isMine={checkForMine(i, j)}
-                    // isRevealed={false}
                     isFlagged={false}
-                    neighbourCount={0}
+                    neighbourCount={getNeighbourCount(i, j)}
                   />
                 );
               })}
--- src/components/MineSweeper/Cell.tsx
+++ src/components/MineSweeper/Cell.tsx
@@ -2,22 +2,18 @@ import './Cell.css';
 import { useState } from 'react';
 
 type CellProps = {
-  x: number;
-  y: number;
   isMine: boolean;
   isFlagged: boolean;
   neighbourCount: number;
 };
 
-const Cell = ({ x, y, isMine, isFlagged, neighbourCount }: CellProps) => {
+const Cell = ({ isMine, isFlagged, neighbourCount }: CellProps) => {
   const [isRevealed, setIsRevealed] = useState(false);
   return (
     <button
       data-testid="board-cell"
       type="button"
-      className={`cell ${isRevealed ? 'revealed' : ''} ${
-        isFlagged ? 'flagged' : ''
-      }`}
+      className="mine-cell"
       onClick={() => setIsRevealed(true)}
     >
       {isRevealed && isFlagged && '🚩'}
