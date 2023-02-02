9f85a3fd86a48a75f4155fccf7d8bb97327119d2
ccorbin <calcorbin@gmail.com>
2023-02-02 16:17:09 -0600

    Get minesweeper Cell component to 100% test coverage

--- src/components/MineSweeper/Cell.tsx
+++ src/components/MineSweeper/Cell.tsx
@@ -1,5 +1,5 @@
 import './Cell.css';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 type CellProps = {
   isMine: boolean;
@@ -8,26 +8,11 @@ type CellProps = {
 
 const Cell = ({ isMine, neighbourCount }: CellProps) => {
   const [isRevealed, setIsRevealed] = useState(false);
-  const [isFlagged, setIsFlagged] = useState(false);
 
   const handleClick = (e: React.MouseEvent) => {
     e.preventDefault();
     setIsRevealed(true);
   };
-
-  const handleContextMenu = (e: React.MouseEvent) => {
-    e.preventDefault();
-    setIsFlagged(!isFlagged);
-  };
-
-  useEffect(() => {
-    document.addEventListener('contextmenu', () => handleContextMenu);
-    document.addEventListener('click', () => handleClick);
-    return () => {
-      document.removeEventListener('contextmenu', () => handleContextMenu);
-      document.removeEventListener('click', () => handleClick);
-    };
-  });
 
   return (
     <button
@@ -36,7 +21,6 @@ const Cell = ({ isMine, neighbourCount }: CellProps) => {
       className={`mine-cell ${isRevealed && 'revealed'}`}
       onClick={(e) => handleClick(e)}
     >
-      {isRevealed && isFlagged && '🚩'}
       {isRevealed && isMine && '💣'}
       {isRevealed && !isMine && neighbourCount > 0 && neighbourCount}
     </button>
