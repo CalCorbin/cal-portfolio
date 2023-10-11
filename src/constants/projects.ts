ef9cc5a8e5e441b7ed434227b243be578ecb834f
ccorbin <calcorbin@gmail.com>
2023-10-11 12:15:53 -0500

    Chore: Comment out Fanwave from projects.ts

--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -20,12 +20,13 @@ const PROJECTS: Array<Project> = [
     link: ROUTES.CHICAGOART,
     img: 'https://images.unsplash.com/flagged/photo-1572392640988-ba48d1a74457?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8YXJ0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=900&q=60',
   },
-  {
-    id: 7,
-    title: 'Fanwave',
-    link: 'https://okc.fanwave.io/',
-    img: 'https://uploads-ssl.webflow.com/618cc38d18a62f370fa48fb4/6190616f493d272fbae5cba9_fanwave%20svg.svg',
-  },
+  // The Fanwave website is dead :(
+  // {
+  //   id: 7,
+  //   title: 'Fanwave',
+  //   link: 'https://okc.fanwave.io/',
+  //   img: 'https://uploads-ssl.webflow.com/618cc38d18a62f370fa48fb4/6190616f493d272fbae5cba9_fanwave%20svg.svg',
+  // },
   {
     id: 6,
     title: 'HexClock',
