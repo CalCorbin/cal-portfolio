15f5c1a30f9fffeb4efe39611a26061e6d64b5e7
ccorbin <calcorbin@gmail.com>
2023-11-02 12:31:41 -0500

    fix: Ensure cards on homepage have pointer cursor

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -12,22 +12,20 @@ interface CardProps {
 }
 
 const Card = ({ data }: CardProps) => (
-  <div className={styles.simpleCard} data-testid={`card-${data.id}`}>
+  <a
+    className={styles.simpleCard}
+    href={data.link}
+    target="_blank"
+    rel="noreferrer"
+  >
     <img
       style={{ width: '90%', margin: '10px' }}
       alt={data.title}
       src={data.img}
       data-testid={`card-image-${data.id}`}
     />
-    <a
-      href={data.link}
-      target="_blank"
-      rel="noreferrer"
-      data-testid={`card-link-${data.id}`}
-    >
-      <div>{data.title}</div>
-    </a>
-  </div>
+    {data.title}
+  </a>
 );
 
 Card.defaultProps = {
