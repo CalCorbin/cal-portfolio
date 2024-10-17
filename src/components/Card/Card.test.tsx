754618eb4a4c2c34a058c1b091323cfc7db26191
ccorbin <calcorbin@gmail.com>
2024-10-17 10:08:08 -0500

    chore: clean up Card tests

--- src/components/Card/Card.test.tsx
+++ src/components/Card/Card.test.tsx
@@ -3,26 +3,48 @@ import { cleanup, render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import Card from './Card';
 
-beforeEach(() => {
-  render(<Card data={mockCard} />);
-});
-afterEach(cleanup);
+describe('Card', () => {
+  const initialProps = {
+    data: {
+      id: 1,
+      title: 'Star Trek Next Generation Elevator Game',
+      link: 'https://github.com/CalCorbin/elevatorGame',
+      img: 'https://images.unsplash.com/photo-1550479023-2a811e19dfd3',
+      hasBackButton: false,
+    },
+  };
 
-const mockCard = {
-  id: 1,
-  title: 'Star Trek Next Generation Elevator Game',
-  link: 'https://github.com/CalCorbin/elevatorGame',
-  img: 'https://images.unsplash.com/photo-1550479023-2a811e19dfd3',
-};
+  const prepareComponent = (props = initialProps) =>
+    render(<Card {...props} />);
+  afterEach(cleanup);
 
-it('should render title', () => {
-  expect(screen.getByText(mockCard.title)).toBeInTheDocument();
-});
+  it('should render title', () => {
+    prepareComponent();
+    expect(screen.getByText(initialProps.data.title)).toBeInTheDocument();
+  });
 
-it('should render image', () => {
-  expect(screen.getByTestId('card-image-1')).toBeInTheDocument();
-});
+  it('should render image', () => {
+    prepareComponent();
+    expect(screen.getByTestId('card-image-1')).toBeInTheDocument();
+  });
 
-it('should render link', async () => {
-  expect(screen.getByRole('link')).toHaveAttribute('href', mockCard.link);
+  it('should render link', async () => {
+    prepareComponent();
+    expect(screen.getByRole('link')).toHaveAttribute(
+      'href',
+      initialProps.data.link
+    );
+  });
+
+  it('should render _self if hasBackButton is true', async () => {
+    prepareComponent({
+      data: { ...initialProps.data, hasBackButton: true },
+    });
+    expect(screen.getByRole('link')).toHaveAttribute('target', '_self');
+  });
+
+  it('should render _blank if hasBackButton is false', async () => {
+    prepareComponent();
+    expect(screen.getByRole('link')).toHaveAttribute('target', '_blank');
+  });
 });
