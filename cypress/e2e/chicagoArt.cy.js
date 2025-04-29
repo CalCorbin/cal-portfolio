ea10d0af10b212311162abffad9c0c3a28a1f35e
ccorbin <calcorbin@gmail.com>
2025-04-29 16:31:27 -0500

    Update ChicagoArt e2e test

--- cypress/e2e/chicagoArt.cy.js
+++ cypress/e2e/chicagoArt.cy.js
@@ -9,11 +9,18 @@ describe('Chicago Art', () => {
 
     // Verify the Chicago Art page loads and open the github code example
     cy.contains('h1', 'Chicago Art Institute Explorer').should('be.visible');
-    cy.findByRole('link', {
-      name: 'View the code for Chicago Art Institute Explorer on GitHub',
-    })
-      .invoke('removeAttr', 'target')
-      .click();
-    cy.assertGithubCodeExampleLoaded();
+
+    // TODO - Add back github link assertion once refactor of Chicago Art is complete
+    // cy.findByRole('link', {
+    //   name: 'View the code for Chicago Art Institute Explorer on GitHub',
+    // })
+    //   .invoke('removeAttr', 'target')
+    //   .click();
+    // cy.assertGithubCodeExampleLoaded();
+
+    // Click input and enter text
+    cy.get('input').type('still life');
+    cy.get('button').click();
+    cy.get('img[alt="A work made of oil on canvas."]').should('be.visible');
   });
 });
