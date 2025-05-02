31f0f253bbd27361de28da4d6af2a78be2a92341
ccorbin <calcorbin@gmail.com>
2025-05-02 16:19:47 -0500

    Lower amount of github page assertions in e2e tests

--- cypress/e2e/hexClock.cy.js
+++ cypress/e2e/hexClock.cy.js
@@ -6,12 +6,6 @@ describe('HexClock', () => {
   it('should visit the HexClock project and view the github code example', () => {
     // Open the HexClock project
     cy.contains('a', 'HexClock').click();
-
-    // Verify the HexClock page loads and open the github code example
     cy.contains('h1', 'HexClock').should('be.visible');
-    cy.findByRole('link', { name: 'View the code for HexClock on GitHub' })
-      .invoke('removeAttr', 'target')
-      .click();
-    cy.assertGithubCodeExampleLoaded();
   });
 });
--- cypress/e2e/spaceX.cy.js
+++ cypress/e2e/spaceX.cy.js
@@ -13,15 +13,7 @@ describe('SpaceX', () => {
     () => {
       // Open the SpaceX page
       cy.contains('a', 'SpaceX GraphQL').click();
-
-      // Verify the SpaceX page loads and open the github code example
       cy.contains('h1', 'SpaceX Marine Transport Ships').should('be.visible');
-      cy.findByRole('link', {
-        name: 'View the code for SpaceX Marine Transport Ships on GitHub',
-      })
-        .invoke('removeAttr', 'target')
-        .click();
-      cy.assertGithubCodeExampleLoaded();
     }
   );
 });
